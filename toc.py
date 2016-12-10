d55e79f1d9b541fbcd7f55af1e5ba4ac3a572589
liliqing <llq123go@163.com>
2016-12-10 11:50:48 +0800

    生成目录的python程序

---
+++ toc.py
@@ -0,0 +1,56 @@
+import os
+import urllib.parse
+
+ignore_dir_or_file = ['.git', 'README.md']
+
+GIT_TREE_URL = 'https://github.com/cangsangyuemanlou/codenote/tree/master'
+GIT_BLOB_URL = 'https://github.com/cangsangyuemanlou/codenote/blob/master'
+
+root_path = 'E:/test/python'
+space_char = '&emsp;'
+
+
+def go_through_path(basepath, deep=0, parents=''):
+    all_files = []
+
+    all_files_list = list(filter(lambda f: f not in ignore_dir_or_file, os.listdir(basepath)))
+
+    dirflt = filter(lambda f: os.path.isdir(os.path.join(basepath, f)), all_files_list)
+    for d in dirflt:
+        all_files.append(d)
+
+    files = sorted(filter(lambda f: os.path.isfile(os.path.join(basepath, f)), all_files_list),
+                   key=lambda f: os.path.getctime(os.path.join(basepath, f)))
+    for fl in files:
+        all_files.append(fl)
+
+    for file in all_files:
+        filepath = os.path.join(basepath, file)
+        if os.path.isdir(filepath):
+            yield (1, deep, parents, file)  # dir
+            yield from go_through_path(filepath, deep + 1, '/'.join([parents, file]))
+        else:
+            yield (0, deep, parents, file)  # file
+
+
+def get_git_conent():
+    for info in go_through_path(root_path):
+        filepath = '/'.join(info[2:])
+        filepath = urllib.parse.quote(filepath)
+        if info[0] == 1:
+            giturl = GIT_TREE_URL + filepath
+        else:
+            giturl = GIT_BLOB_URL + filepath
+        yield (space_char * info[1] + info[3], giturl)
+
+
+def write_readme():
+    readme_file_path = root_path + '/README.md'
+    with open(readme_file_path, 'w', encoding='utf-8') as fp:
+        for item in get_git_conent():
+            fp.write("[{content}]({link})\n".format(content=item[0], link=item[1]))
+
+
+if __name__ == '__main__':
+    root_path = os.path.abspath('.')
+    write_readme()
