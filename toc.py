5aaf6d8ada66e61cb772df436dade368d42b0bf9
liliqing <llq123go@163.com>
2016-12-13 19:16:02 +0800

    toc

--- toc.py
+++ toc.py
@@ -1,7 +1,7 @@
 import os
 import urllib.parse
 
-ignore_dir_or_file = ['.git', 'README.md', 'toc.py']
+ignore_dir_or_file = ['.git', 'README.md', 'toc.py', 'code']
 
 GIT_TREE_URL = 'https://github.com/cangsangyuemanlou/codenote/tree/master'
 GIT_BLOB_URL = 'https://github.com/cangsangyuemanlou/codenote/blob/master'
